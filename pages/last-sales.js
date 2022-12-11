d4c2f0e9c11403fe82a77c1d4d88078421996baa
Michael Mburu <ichangaimichael@yahoo.com>
2022-12-11 15:47:12 +0300

    Using SWR with get static props

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,8 +1,30 @@
 import { useEffect, useState } from 'react'
+import useSWR from 'swr'
 
-const LastSalesPage = () => {
-  const [sales, setSales] = useState()
-  const [isLoading, setIsLoading] = useState(false)
+const LastSalesPage = (props) => {
+  const [sales, setSales] = useState(props.sales)
+  //const [isLoading, setIsLoading] = useState(false)
+
+  const { data, error } = useSWR(
+    'https://nextjs-course-e3831-default-rtdb.firebaseio.com/sales.json'
+  )
+
+  useEffect(() => {
+    if (data) {
+      const transformedSales = []
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        })
+      }
+      setSales(transformedSales)
+    }
+
+  }, [data])
+
+  /*
   useEffect(() => {
     setIsLoading(true)
     fetch('https://nextjs-course-e3831-default-rtdb.firebaseio.com/sales.json')
@@ -22,21 +44,44 @@ const LastSalesPage = () => {
         setIsLoading(false)
       })
   }, [])
+  */
 
-  if(!sales){
-    return <p>No sales</p>
+  if (error) {
+    return <p>Failed to load</p>
   }
-  return isLoading ? (
-    <p>Is Loading.....</p>
-  ) : (
-    <ul>
-      {sales.map(sale => (
-      <li key={sale.id}>
-        {sale.username} - ${sale.volume}
-      </li>
+
+  if (!data && !sales) {
+    <p>Loading....</p>
+  }
+
+  return (
+    sales && <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.username} - ${sale.volume}
+        </li>
       ))}
     </ul>
   )
+}
+
+export const getStaticProps = async () => {
+    return fetch('https://nextjs-course-e3831-default-rtdb.firebaseio.com/sales.json')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        const transformedSales = []
+        for (const key in data) {
+          transformedSales.push({
+            id: key,
+            username: data[key].username,
+            volume: data[key].volume,
+          })
+        }
+        
+        return {props: {sales: transformedSales}}
+      })
+  
 }
 
 export default LastSalesPage
